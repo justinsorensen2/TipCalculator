81014d46cbf4611f8472b15f2487f58de6166a47
justinsorensen2 <justinsorensen2@gmail.com>
2020-02-10 14:38:59 -0500

    Created first version

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TipCalculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // welcome user
+      Console.WriteLine("Welcome User!");
+      //ask for bill total
+      Console.WriteLine("How much was your meal?");
+      var billSansTip = Console.ReadLine();
+      //calculate 18%
+      var tip = double.Parse(billSansTip) * 0.18;
+      var total = double.Parse(billSansTip) + tip;
+      //display the result
+      Console.WriteLine("Your bill was $" + billSansTip + ".");
+      Console.WriteLine("An 18% tip would be $" + tip + ".");
+      Console.WriteLine("Your total will be $" + total + ".");
+
+    }
+  }
+}
