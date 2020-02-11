cc05e55d62801c841a006c4fa928b5a509468670
justinsorensen2 <justinsorensen2@gmail.com>
2020-02-11 11:02:09 -0500

    Added service level for tip.

--- Program.cs
+++ Program.cs
@@ -8,15 +8,39 @@ namespace TipCalculator
     {
       // welcome user
       Console.WriteLine("Welcome User!");
-      //ask for bill total
+      //ask for bill total and run error correction
       Console.WriteLine("How much was your meal?");
       var billSansTip = Console.ReadLine();
-      //calculate 18%
-      var tip = double.Parse(billSansTip) * 0.18;
+      //ask how service
+      Console.WriteLine("How was your service?");
+      Console.WriteLine("Valid choices are okay, good, and great.");
+      var service = Console.ReadLine();
+      //ok, good, great, default 
+      //calculate tip based on service
+      var tipPercentage = 0.15;
+      if (service == "okay")
+      {
+        tipPercentage = 0.18;
+      }
+      else if (service == "good")
+      {
+        tipPercentage = 0.20;
+      }
+      else if (service == "great")
+      {
+        tipPercentage = 0.25;
+      }
+      else
+      {
+        tipPercentage = 0.15;
+      }
+      var tip = double.Parse(billSansTip) * tipPercentage;
       var total = double.Parse(billSansTip) + tip;
+      //multiply tip percent by 100
+      var TipToDisplay = tipPercentage * 100;
       //display the result
       Console.WriteLine("Your bill was $" + billSansTip + ".");
-      Console.WriteLine("An 18% tip would be $" + tip + ".");
+      Console.WriteLine(TipToDisplay + "% tip would be $" + tip + ".");
       Console.WriteLine("Your total will be $" + total + ".");
 
     }
